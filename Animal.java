9cb7106c00b038b79d77f6ceaa6e4d7745e7de3e
Lee Xiu Niang <55247305+LeeXiuNiang@users.noreply.github.com>
2020-07-09 16:26:50 +0800

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Animal {
+
+    public int age;
+
+    public void introduceYourself(){
+        System.out.println("Morr. I am an animal.");
+        System.out.println("and I am " + this.age + " years old.");
+    }
+}
---
+++ Cat.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Cat extends Animal{
+
+    private String name;
+
+    public Cat(String name) {
+        this.name=name;
+    }
+
+    public void introduceYourself(){
+        System.out.println("Meow. I am a cat. My name is " + this.name);
+        System.out.println("and I am " + this.age + " years old.");
+    }
+}
